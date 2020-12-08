394f018f4d366f71cedd0ea3170141f3de083078
Graham Squires <graham.squires3@gmail.com>
2020-12-08 14:39:02 -0500

    Delete Button Completed for frontend

--- src/SongList.js
+++ src/SongList.js
@@ -12,8 +12,8 @@ function SongList(props){
             <Card.Description>Album: {song.album}</Card.Description>
           </Card.Content>
           <Card.Content extra>
-            <Button>DeleteSong</Button>
-            <Button>Edit Song</Button>
+          <Button onClick={() => props.deleteSong(song.id)}>Delete Song</Button>
+            <Button>Edit Song (Needs Done)</Button>
           </Card.Content>
         </Card>
         )
