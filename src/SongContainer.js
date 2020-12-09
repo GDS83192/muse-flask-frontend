50a20d19ebb494fec8220bb3b374ead284142305
Graham Squires <graham.squires3@gmail.com>
2020-12-09 14:32:18 -0500

    edit song functionality added for front end by adding edit modal

--- src/SongContainer.js
+++ src/SongContainer.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SongList from './SongList';
-import CreateSongForm from './CreateSongForm'
+import CreateSongForm from './CreateSongForm';
+import EditSongModal from './EditSongModal';
+import { Grid } from 'semantic-ui-react';
+
 class SongContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      songs: [],
-    };
-  }
+  state = {
+    songs: [],
+    songToEdit: {
+      title: '',
+      artist: '',
+      album: '',
+    },
+    showEditModal: false,
+  };
+
   componentDidMount() {
     this.getSongs();
   }
@@ -25,20 +32,21 @@ class SongContainer extends Component {
       console.log(err);
     }
   };
+
   addSong = async (e, song) => {
     e.preventDefault();
     console.log(song);
 
     try {
-      // The createdSongResponse variable will store the response from the Flask API
+      // The createdDogResponse variable will store the response from the Flask API
       const createdSongResponse = await axios.post(
         process.env.REACT_APP_FLASK_API_URL + '/api/v1/songs/',
         song
       );
 
-      // we are emptying all the songs that are living in state into a new array,
-      // and then adding the song we just created to the end of it
-      // the new song which is called parsedResponse.data
+      // we are emptying all the dogs that are living in state into a new array,
+      // and then adding the dog we just created to the end of it
+      // the new dog which is called parsedResponse.data
 
       console.log(createdSongResponse.data.data, ' this is response');
       this.setState({
@@ -56,21 +64,93 @@ class SongContainer extends Component {
     );
     console.log(deleteSongResponse);
     // Now that the db has deleted our item, we need to remove it from state
-    // Then make the delete request, then remove the song from the state array using filter
+    // Then make the delete request, then remove the dog from the state array using filter
     this.setState({ songs: this.state.songs.filter((song) => song.id !== id) });
 
     console.log(deleteSongResponse, ' response from Flask server');
   };
-  
-  // rest of code
 
- render(){
+  openAndEdit = (songFromTheList) => {
+    console.log(songFromTheList, ' songToEdit  ');
+
+    this.setState({
+      showEditModal: true,
+      songToEdit: {
+        ...songFromTheList,
+      },
+    });
+  };
+
+  handleEditChange = (e) => {
+    this.setState({
+      songToEdit: {
+        ...this.state.songToEdit,
+        [e.currentTarget.name]: e.currentTarget.value,
+      },
+    });
+  };
+
+  closeAndEdit = async (e) => {
+    e.preventDefault();
+    try {
+      const editResponse = await axios.put(
+        process.env.REACT_APP_FLASK_API_URL +
+          '/api/v1/songs/' +
+          this.state.songToEdit.id,
+        this.state.songToEdit
+      );
+
+      console.log(editResponse, ' parsed edit');
+
+      const newSongArrayWithEdit = this.state.songs.map((song) => {
+        if (song.id === editResponse.data.data.id) {
+          song = editResponse.data.data;
+        }
+
+        return song;
+      });
+
+      this.setState({
+        showEditModal: false,
+        songs: newSongArrayWithEdit,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
     return (
-      <>
-        <SongList songs={this.state.songs} deleteSong={this.deleteSong}/>
-        <CreateSongForm addSong={this.addSong}/>
-      </>
-      )
+      <Grid
+        columns={2}
+        divided
+        textAlign="center"
+        style={{ height: '100%' }}
+        verticalAlign="top"
+        stackable
+      >
+        <Grid.Row>
+          <Grid.Column>
+            <SongList
+              songs={this.state.songs}
+              deleteSong={this.deleteSong}
+              openAndEdit={this.openAndEdit}
+              handleEditChange={this.handleEditChange}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <CreateSongForm addSong={this.addSong} />
+          </Grid.Column>
+          <EditSongModal
+            handleEditChange={this.handleEditChange}
+            open={this.state.showEditModal}
+            songToEdit={this.state.songToEdit}
+            closeAndEdit={this.closeAndEdit}
+          />
+        </Grid.Row>
+      </Grid>
+    );
   }
 }
+
 export default SongContainer;
